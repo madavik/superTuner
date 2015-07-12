cb76f8520420030eaeae43d7f371cd0be3e96564
joey gibson <joe.vgibson@gmail.com>
2015-07-12 16:39:58 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperTuner::Application.config.secret_token = 'ae5d11342c4a45f5c030d4e79af08701a398957a1ac7ba41684769dd21bbe6f83d0279aaf3ed4ab58857fd39a23290d4cc5f046518049b7312992433debf8383'
