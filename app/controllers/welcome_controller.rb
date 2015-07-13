3ee7e865eec3c150730e52357093140c4ea95a0c
joey gibson <joe.vgibson@gmail.com>
2015-07-12 20:14:55 -0600

    team page work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def team
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SuperTuner::Application.routes.draw do
   get "welcome/index"
   root :to => 'welcome#index'
+  match '/team' => 'welcome#team'
 end
